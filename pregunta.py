be7177b66eafefcd9d3b4ab417ecd1b0376e364a
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-23 17:09:14 -0500

    Create pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -8,12 +8,13 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-df = pd.read_csv("solicitudes_credito.csv",delimiter = ';')
 
 def clean_data():
 
     df.dropna(inplace=True)
     #df.drop_duplicates(inplace=True)
+    
+    
     df["sexo"] =  df["sexo"].map(lambda x: str(x).lower().strip())  #pasa a minusculas
     
     df["tipo_de_emprendimiento"] =  df["tipo_de_emprendimiento"].apply(lambda y: str(y).lower().strip())  #pasa a minusculas
@@ -21,14 +22,10 @@ def clean_data():
     df["idea_negocio"] = df["idea_negocio"].map(lambda y: str(y).lower().strip()) #pasa a minusculas 
     df["idea_negocio"] = df["idea_negocio"].apply(lambda x : str(x).replace("-"," ").strip())
     df["idea_negocio"] = df["idea_negocio"].apply(lambda x : str(x).replace("_"," ").strip())
-    
-    #df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
 
     df["barrio"] = df["barrio"].apply(lambda y: str(y).lower()) #pasa a minusculas 
     df["barrio"] = df["barrio"].apply(lambda x : str(x).replace("-"," "))
     df["barrio"] = df["barrio"].apply(lambda x : str(x).replace("_"," "))
-
-    #df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().replace("_"," ").replace("-"," "))##revisar
 
     df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"],dayfirst=True)
     
